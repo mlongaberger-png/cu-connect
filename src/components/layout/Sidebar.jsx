ecd1bb48e8c1adecee90eab492420afc77d006d0
jcummins <jcummins@cornerstoneunitedlions.com>
2026-03-18 18:29:26 +0000

    File changes

--- src/components/layout/Sidebar.jsx
+++ src/components/layout/Sidebar.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { Link, useLocation } from "react-router-dom";
 import {
   LayoutDashboard, Trophy, Users, Calendar, MessageSquare,
-  Megaphone, FolderOpen, UserCircle, X, Shield } from
+  FolderOpen, UserCircle, X } from
 "lucide-react";
 
 const navItems = [
