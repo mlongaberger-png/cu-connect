4dc73fb21561d09d6033c29481bf44415844be18
Matthew Longaberger <mlongaberger@cornerstoneunitedlions.com>
2026-04-06 18:36:47 +0000

    File changes

--- src/pages/Messages.jsx
+++ src/pages/Messages.jsx
@@ -373,7 +373,7 @@ export default function Messages() {
           </div>
         </div>
 
-        {/* Messages + Attendance Cards */
+        {/* Messages + Attendance Cards */}
         <div ref={messagesScrollRef} className="flex-1 overflow-y-auto p-4 md:p-6 space-y-4">
           {refreshing && (
             <div className="flex justify-center py-2">
